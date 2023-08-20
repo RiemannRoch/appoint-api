aa94d6492757d237afa03bed11cc925f8278bac0
riemannroch <riemannroch@github.com>
2023-08-20 10:21:32 -0300

    Initial commit

---
+++ src/main/java/com/riemannroch/appointapi/AppointApiApplication.java
@@ -0,0 +1,13 @@
+package com.riemannroch.appointapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riemannroch/appointapi/AppointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riemannroch.appointapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
